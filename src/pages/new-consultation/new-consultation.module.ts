116012828f546c3020a430a97a6b5d4e4d176f6b
Dima Katsiuba <dzmitry.katsiuba@uzh.ch>
2018-07-03 09:45:51 +0200

    Changes from yesterday

--- src/pages/new-consultation/new-consultation.module.ts
+++ src/pages/new-consultation/new-consultation.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { NewConsultationPage } from './new-consultation';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     NewConsultationPage,
   ],
   imports: [
+    HttpClientModule,
     IonicPageModule.forChild(NewConsultationPage),
   ],
 })
 export class NewConsultationPageModule {}
+git
--- src/pages/new-consultation/new-consultation.ts
+++ src/pages/new-consultation/new-consultation.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {HttpClient} from "@angular/common/http";
+import {HttpHeaders} from "@angular/common/http";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content‐Type': 'application/json',
+    'x‐apikey': '5b2e750b0c346a20d90a5dda'
+  })
+};
 
 /**
  * Generated class for the NewConsultationPage page.
@@ -17,6 +26,8 @@ export class NewConsultationPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
+
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NewConsultationPage');
